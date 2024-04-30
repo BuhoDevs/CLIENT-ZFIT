56e6301cc04474f122b878adcfdacecb1ea5e452
josemaria <devfarm.cheyo@gmail.com>
2024-04-30 01:02:32 -0400

    update subscription valores default desde back

--- src/hooks/subscriptions.ts
+++ src/hooks/subscriptions.ts
@@ -1,8 +1,10 @@
-import { useMutation } from "@tanstack/react-query";
+import { useMutation, useQuery } from "@tanstack/react-query";
 import {
   getSubscriptionsByFilters,
+  getSubscriptionsById,
   insertSubscription,
 } from "../services/subscriptions/subscriptions.service";
+import { ISubscriptionByIdParams } from "../types/suscription";
 
 export const usePostSubscription = () => {
   return useMutation({
@@ -15,5 +17,18 @@ export const useGetSubscriptionsByFilters = () => {
   return useMutation({
     mutationFn: getSubscriptionsByFilters,
     mutationKey: ["subscriptions-filters"],
+  });
+};
+
+export const useGetSubscriptionsById = ({
+  id,
+  isReadyToFetch = false,
+}: ISubscriptionByIdParams) => {
+  return useQuery({
+    queryFn: () => getSubscriptionsById({ id }),
+    queryKey: ["subscriptions-by-id", { id }],
+    retry: 1,
+    refetchOnWindowFocus: false,
+    enabled: isReadyToFetch,
   });
 };
--- src/services/subscriptions/subscriptions.service.ts
+++ src/services/subscriptions/subscriptions.service.ts
@@ -3,10 +3,13 @@ import {
   IBodySuscription,
   IGetSubscriptionsPromise,
   IPostResponse,
+  ISubscriptionByIdParams,
+  ISubscriptionByIdResponse,
   ISubscriptionFilters,
 } from "../../types/suscription";
 import {
   insertSubscriptionEndpoint,
+  subscriptionsByIdEndpoint,
   subscriptionsFiltersEndpoint,
 } from "./subscriptions.endpoints";
 import { subscriptionFiltersParsed } from "./utils";
@@ -29,5 +32,15 @@ export const getSubscriptionsByFilters = async ({
     subscriptionsFiltersEndpoint(),
     subscriptionFiltersParsed({ subscriptionData, skip, take })
   );
+  return data;
+};
+
+export const getSubscriptionsById = async ({
+  id,
+}: ISubscriptionByIdParams): Promise<ISubscriptionByIdResponse> => {
+  const { data } = await axiosApi.get<ISubscriptionByIdResponse>(
+    subscriptionsByIdEndpoint({ id })
+  );
+
   return data;
 };
