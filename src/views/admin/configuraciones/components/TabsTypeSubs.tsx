e662179f0b368bdb4bccc5af5a117acd3dbdd887
jerlibgnzlz <jerlibgnzlz@gmail.com>
2024-05-08 17:25:31 -0300

    fix estructura para vista configuracion

--- src/views/admin/configuraciones/components/TabsTypeSubs.tsx
+++ src/views/admin/configuraciones/components/TabsTypeSubs.tsx
@@ -1,71 +1,64 @@
-
-import { useState } from 'react';
-import {
-    Box,
-    Button,
-    Input,
-    useToast,
-} from '@chakra-ui/react';
-import { useDeleteSubscription } from '../../../../hooks/subscriptionsType';
+import { useState } from "react";
+import { Box, Button, Input, useToast } from "@chakra-ui/react";
+import { useDeleteSubscription } from "../../../../hooks/subscriptionsType";
 
 const DeleteSubscriptionComponent = () => {
-    const [subscriptionId, setSubscriptionId] = useState('');
-    const { mutateAsync, isLoading } = useDeleteSubscription();
-    const toast = useToast();
+  const [subscriptionId, setSubscriptionId] = useState("");
+  const { mutateAsync } = useDeleteSubscription();
+  const toast = useToast();
+  const [isLoading, setIsLoading] = useState(false);
 
-    const handleDelete = async () => {
-        if (!subscriptionId)
-        {
-            toast({
-                title: 'Error',
-                description: 'Por favor, introduce un ID de suscripción',
-                status: 'error',
-                duration: 3000,
-                isClosable: true,
-            });
-            return;
-        }
+  const handleDelete = async () => {
+    if (!subscriptionId) {
+      toast({
+        title: "Error",
+        description: "Por favor, introduce un ID de suscripción",
+        status: "error",
+        duration: 3000,
+        isClosable: true,
+      });
+      return;
+    }
 
-        try
-        {
-            await mutateAsync(parseInt(subscriptionId));
-            toast({
-                title: 'Éxito',
-                description: 'La suscripción se eliminó correctamente',
-                status: 'success',
-                duration: 3000,
-                isClosable: true,
-            });
-            setSubscriptionId('');
-        } catch (error)
-        {
-            console.error('Error al eliminar la suscripción:', error);
-            toast({
-                title: 'Error',
-                description: 'Hubo un problema al eliminar la suscripción',
-                status: 'error',
-                duration: 3000,
-                isClosable: true,
-            });
-        }
-    };
+    setIsLoading(true);
 
-    return (
-        <Box>
-            <Input
-                placeholder="ID de la suscripción"
-                value={subscriptionId}
-                onChange={(e) => setSubscriptionId(e.target.value)}
-                mb={4}
-            />
-            <Button colorScheme="red" onClick={handleDelete} isLoading={isLoading}>
-                Eliminar Suscripción
-            </Button>
-        </Box>
+    try {
+      await mutateAsync(parseInt(subscriptionId));
+      toast({
+        title: "Éxito",
+        description: "La suscripción se eliminó correctamente",
+        status: "success",
+        duration: 3000,
+        isClosable: true,
+      });
+      setSubscriptionId("");
+    } catch (error) {
+      console.error("Error al eliminar la suscripción:", error);
+      toast({
+        title: "Error",
+        description: "Hubo un problema al eliminar la suscripción",
+        status: "error",
+        duration: 3000,
+        isClosable: true,
+      });
+    } finally {
+      setIsLoading(false);
+    }
+  };
 
-
-
-    );
+  return (
+    <Box>
+      <Input
+        placeholder="ID de la suscripción"
+        value={subscriptionId}
+        onChange={(e) => setSubscriptionId(e.target.value)}
+        mb={4}
+      />
+      <Button colorScheme="red" onClick={handleDelete} isLoading={isLoading}>
+        Eliminar Suscripción
+      </Button>
+    </Box>
+  );
 };
 
 export default DeleteSubscriptionComponent;
